6c9f944902e1cc6d02e742d5ac8507f77d892708
Kaisar Arkhan (Yuki) <ykno@protonmail.com>
2017-03-22 20:42:01 +0700

    deploy.sh: Set push behaviour to simple

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ echo "Setting up git identity ..."
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 
+echo "Setting push behaviour to simple ..."
+git config --global push.default simple
+
 echo "Cloning repository ..."
 git clone --branch=gh-pages git@github.com:loklak/dev.loklak.org $HOME/pages
 cd $HOME/pages
